7cc54df06ca98479dd2c60ce8feef5a55ce44a76
Anthony Zeitlin <zeitlin1@gmail.com>
2016-08-27 17:51:59 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -105,12 +105,39 @@ describe('flow-control', () => {
       expect(switchAge(75)).toEqual("You have an age")
       expect(switchAge(7)).toEqual("You have an age")
     })
-  })
-  function switchAge(age) {
-    if (age > 12 && age < 20)
+
+    function switchAge(age) {
+    switch(age) {
+      case 13:
       return ("You are a teenager")
-    else if (age < 13 || age > 19)
-             ("You are not a teenager")
-    return   ("You have an age")
-}
+      break;
+      case 14:
+      return ("You are a teenager")
+      break;
+      case 15:
+      return ("You are a teenager")
+      break;
+      case 16:
+      return ("You are a teenager")
+      break;
+      case 17:
+      return ("You are a teenager")
+      break;
+      case 18:
+      return ("You are a teenager")
+      break;
+      case 19:
+      return ("You are a teenager")
+      break;
+      case age > 19:
+      return ("You are not a teenager")
+      break;
+      case age < 13:
+      return ("You are not a teenager")
+      break;
+      default:
+      return ("You have an age")
+       }
+     }
+})
 })
