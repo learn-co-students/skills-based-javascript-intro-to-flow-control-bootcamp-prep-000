6244fec38c2eb340404c01ba1e3ba4281e8103f6
Dharma theja Navuluri <dharma786theja@aol.com>
2016-09-03 17:28:16 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,17 +23,17 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return (age > 13 && age < 19) ? "You are a teenager" :  "You are not a teenager"
+  return (age > 12 && age < 20) ? "You are a teenager" :  "You are not a teenager"
 }
 
 function switchAge(age) {
     switch (age) {
       case 13:
-        return "You are a teenager"
+        return 'You are a teenager'
       case 14:
-        return "You are a teenager"
+        return 'You are a teenager'
       case 15:
-        return "You are a teenager"
+        return 'You are a teenager'
       case 16:
         return "You are a teenager"
       case 17:
