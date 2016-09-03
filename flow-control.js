5cc8ba9d48463cbb44a8d5af17364e89fa7bd98d
Aziza Akhmedova <azizakhmed@gmail.com>
2016-09-03 01:32:30 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,16 @@
 function basicTeenager(age) {
-
+ if (age >= 13 && age <= 19)
+  return "You are a teenager!"
 }
 
 function teenager(age) {
-
+if ("basicTeenager")
+return "You are a teenager!"
+}
+if ("teenager") {
+  return "you are not a teenager" }
+}  else {
+  return "you are not a teenager"
 }
 
 function ageChecker(age) {
