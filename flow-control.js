96872c8fd1710fee9b565f23dd41d6551e29d4a9
James Appleton <jappleton101@gmail.com>
2016-09-08 03:18:10 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,9 @@ function ageChecker(age) {
   }
 }
 
-
+function ternaryTeenager(age) {
+  return age >= 13 && age <=19 : "You are a teenager" ? "You are not a teenager";
+}
 
 function switchAge(age) {
   switch(age) {
