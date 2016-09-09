2ae1406c5d656f1d9582b54186d0368ea0d06abf
Phuong Do <donamphuong.98@gmail.com>
2016-09-09 14:07:33 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,33 @@
 function basicTeenager(age) {
-
+  return age <= 19 && age >= 13 ? "You are a teenager!" : undefined;
 }
 
 function teenager(age) {
-
+  if(age <= 19 && age >= 13) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if(age <= 19 && age >= 13) {
+    return "You are a teenager!";
+  } else if(age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
-
+  if(age <= 19 && age >= 13) {
+    return "You are a teenager";
+  } else {
+    return "You have an age";
+  }
 }
