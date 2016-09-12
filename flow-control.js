141d53a1971e93cabc9d5c94076ab7fc40c71ba8
David Wistocki <david@davidwistocki.com>
2016-09-12 00:18:08 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,10 @@ function ageChecker(age) {
   }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && age <= 19)
+  return "You are a teenager"
+    if (age < 13 || age > 19)
+    return "You are not a teenager"
 }
 
 function switchAge(age) {
