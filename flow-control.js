7f87c18fc8f7df0ace1eaa4d9ce6a1954e999ba2
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-09-12 04:07:52 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else {
+	return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+    /* moved if up to same line as else, if you are checking other statements after the first if, use else if*/
+  } else if (age <= 12) {
+      return "You are a kid"
+    /* you can either do else with no conditional statement to check or else if like here */
+    } else if (age > 19) {
+	return "You are a grownup"
+  }
 }
+/* Above is working fine */
 
 function ternaryTeenager(age) {
-
+  return age >= 13 && age <= 19 ? "You are a teenager": "You are not a teenager";
 }
 
+/* Switch, 'compiled' */
 function switchAge(age) {
-
+  switch(true) {
+    case (age >= 13 && age <= 19):
+      return "You are a teenager";
+    default:
+      return "You have an age";
+  }
 }
