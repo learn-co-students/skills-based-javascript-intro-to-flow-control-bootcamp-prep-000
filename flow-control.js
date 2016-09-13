bc52a976ab302e52b980b0ffd52388ca2d94919c
Mandy Agwatu <mandyagwatu@gmail.com>
2016-09-13 23:38:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,24 @@
-function basicTeenager(age) {
+function basicTeenager(13-19) {
 
 }
 
 function teenager(age) {
-
+  if basicTeenager() {
+    return("You are a teenager!")
+  } else {
+    return("You are not a teenager")
+  }
 }
 
-function ageChecker(age) {
-
+function ageChecker(age){
+  if (age >= 13 ){
+  return("You are a teenager!")
+} else if (age <= 19) {
+  return("you are a kid")
+} else {
+  return("You are a grownup")
 }
-
+}
 function ternaryTeenager(age) {
 
 }
