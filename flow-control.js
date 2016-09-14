58b579d8d8b5fb9bb5bdc5ddb6358420fd3602ab
Mandy Agwatu <mandyagwatu@gmail.com>
2016-09-14 15:27:19 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,15 +25,20 @@ return "You are a grownup"
 }
 }
 
-function switchAge(age) {
-if (age > 12 && age < 20) {
-return "You are a teenager"
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
+    return "You have an age"
+  }
 }
-else {
-  return "You have an age"
-}
-}
-
 function ternaryTeenager(age) {
   if (age > 12 && age < 20) {
     return "You are a teenager"
