fd1565a0b338073d9561cfe80c80a5609c8701f1
Benjamin Paar <bpaar1@gmail.com>
2016-09-17 18:53:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,10 @@
 function basicTeenager(age) {
-
+  if(age >= 13 && age <= 19) {
+    console.log("You are a teenager!")
+  }
+  else {
+    console.log("You are not a teenager!")
+  }
 }
 
 function teenager(age) {
