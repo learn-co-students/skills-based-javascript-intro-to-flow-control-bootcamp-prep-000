0bd04fb40edd6046f654e45c0a2d83842d98ec21
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2016-09-17 00:27:33 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
-function basicTeenager(age) {
+
+var basicTeenager = function(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+}
+}
+
+var teenager = function(age) {
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager!'
+  } else {
+    return 'You are not a teenager'
+  }
+}
+
+var ageChecker = function(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!"
+  } else if (age <= 12){
+    return "You are a kid"
+  } else if (age >= 20){
+    return "You are a grownup"
+  }
 
 }
 
-function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
+var ternaryTeenager = function(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else if (age <= 12 || age >= 20){
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <= 19){
+    return "You are a teenager"
+  } else {
+    return "You have an age"
+  }
 }
