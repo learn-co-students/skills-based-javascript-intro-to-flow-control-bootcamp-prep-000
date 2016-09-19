77649fab6b1b2d924c9a88779e87f27e1ae5bdde
Tayari Appiah <cjhhh.ta@gmail.com>
2016-09-19 19:12:36 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,47 +24,40 @@ else if(age <= 12){
   return "You are a kid";
   }
 else{
-  return ("You are a grownup");
+  return "You are a grownup";
 }
 
 }
 
 function ternaryTeenager(age) {
-  age >= 13 && age <= 19 ? return("You are a teenager") : return("You are not a teenager")
+  age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
 function switchAge(age) {
   switch(age){
     case 13:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 14:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 15:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 16:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 17:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 18:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
     case 19:
-        return("You are a teenager");
+        return "You are a teenager";
         break;
    default:
       return "You have an age"
   }
 }
-
-
-basicTeenager(13);
-teenager(13);
-ageChecker(12);
-ternaryTeenager(13);
-switchAge(13);
