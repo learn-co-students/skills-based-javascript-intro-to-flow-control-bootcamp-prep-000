7280921a0124569f31fced79f9ad0a9dffa32dc4
Kate Hook <glenn_kt@hotmail.com>
2016-09-27 04:11:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,37 @@
 function basicTeenager(age) {
 
+  if (age > 12 && < 20) {
+  return ("You are a Teenager!");
 }
 
-function teenager(age) {
 
+
+function teenager (age) {
+
+ if (age > 12 && age < 20)
+return ("You are a teenager!");
+} else {
+  return ("You are not a teenager");
 }
+}
+
+
 
 function ageChecker(age) {
 
+  if (age > 12 && age < 20) {
+  return ("You are a teenager!");
+} else if (age < 13) {
+  return ("You are a kid");
+} else if (age < 19) {
+ return ("You are a grownup");
+  }
 }
 
 function ternaryTeenager(age) {
-
+(age >12 && <20) ? "You are a Teenager!" : "You are not a Teenager"
 }
+
 
 function switchAge(age) {
 
