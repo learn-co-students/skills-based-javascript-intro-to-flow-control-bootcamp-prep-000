582f9ab0fe96272893e1ce4ef7aa3e92dfb5f71c
Clark moss <cmoss@mail2business.com>
2016-09-28 01:35:21 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,115 @@
-function basicTeenager(age) {
 
+function basicTeenager(age){
+ if (age >= 13 && age <=19) {
+ return "You are a teenager!"
 }
+ }
 
-function teenager(age) {
 
+ function teenager(age) {
+   if (age >= 13 && age <=19) {
+   return "You are a teenager!"
+  }
+  else {
+   return"You are not a teenager"
+   }
 }
-
 function ageChecker(age) {
+  switch(age){
+     case (13):
+        return "You are a teenager!"
+        break;
+    case (14) :
+       return "You are a teenager!"
+        break;
+    case (15) :
+        return  "You are a teenager!"
+        break;
+    case (16) :
+        return "You are a teenager!"
+        break;
+   case (17) :
+      return  "You are a teenager!"
+      break;
+   case (18) :
+      return  "You are a teenager!"
+     break;
+  case (19) :
+      return  "You are a teenager!"
+    break;
+    case (1) :
+          return  "You are a kid"
+        break;
+       case (2) :
+          return  "You are a kid"
+        break;
 
+          case (3) :
+          return  "You are a kid"
+        break;
+          case (4) :
+          return  "You are a kid"
+        break;
+          case (5) :
+          return  "You are a kid"
+        break;
+          case (6) :
+          return  "You are a kid"
+        break;
+          case (7) :
+          return  "You are a kid"
+        break;
+       case (8) :
+          return  "You are a kid"
+        break;
+        case (9) :
+          return  "You are a kid"
+        break;
+        case (10) :
+          return  "You are a kid"
+        break;
+          case (11) :
+          return  "You are a kid"
+        break;
+            case (12) :
+          return  "You are a kid"
+        break;
+
+
+  default: return"You are a grownup"
+
+}
 }
 
 function ternaryTeenager(age) {
+return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  }
 
-}
 
 function switchAge(age) {
+  switch(age){
+   case (13):
+      return "You are a teenager"
+      break;
+  case (14) :
+     return "You are a teenager"
+      break;
+  case (15) :
+      return  "You are a teenager"
+      break;
+  case (16) :
+      return "You are a teenager"
+      break;
+ case (17) :
+    return  "You are a teenager"
+    break;
+ case (18) :
+    return  "You are a teenager"
+   break;
+case (19) :
+    return  "You are a teenager"
+  break;
+default: return "You have an age"
 
+}
 }
