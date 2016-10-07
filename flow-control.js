07bced209fafe1e8f80e6f07b4ca57581c6a2e24
Tavish Harrison <creationmargin26@gmail.com>
2016-10-07 04:01:43 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -19,28 +19,28 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case "13":
-    console.log(return "You are a teenager");
+    case 13:
+    return "You are a teenager";
     break;
-    case "14":
-    console.log(return "You are a teenager");
+    case 14:
+    return "You are a teenager";
     break;
-    case "15":
-    console.log(return "You are a teenager");
+    case 15:
+    return "You are a teenager";
     break;
-    case "16":
-    console.log(return "You are a teenager");
+    case 16:
+    return "You are a teenager";
     break;
-    case "17":
-    console.log(return "You are a teenager");
+    case 17:
+    return "You are a teenager";
     break;
-    case "18":
-    console.log(return "You are a teenager");
+    case 18:
+    return "You are a teenager";
     break;
-    case "19":
-    console.log(return "You are a teenager");
+    case 19:
+    return "You are a teenager";
     break;
     default:
-    console.log("You have an age")
+    return "You have an age"
   }
 }
