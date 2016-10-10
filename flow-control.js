ef40739d795db486e35f3c33a4e65021dd3c7ce9
Murray Elder <elder.murray84@gmail.com>
2016-10-10 19:26:33 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
 function basicTeenager(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!"
+  }
 }
+
 
 function teenager(age) {
-
-}
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager!"
+  }
+]
 
 function ageChecker(age) {
-
-}
+  return  "You are a teenager!""You are a kid!"
+  }
+  else if(age <= 12){
+    return "You are a kid!"
+  }
+  else if(age > 19){
+    return "You are a grown up!"
+  }
+  }
 
 function ternaryTeenager(age) {
-
-}
+  if(return age >= 13 && age <= 19 ? "You are a teenager!" :
+   "You are not a teenager!")
+  }
 
 function switchAge(age) {
-
-}
+  switch(age){
+    case '>=13 && <=19':
+    return "You are a teenager!"
+    break;
+    default:
+    return "You have an age!"
+  }
+  }
