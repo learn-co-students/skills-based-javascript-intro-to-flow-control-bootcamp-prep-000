dbd23e60d1598c13b25d4a66a139ef6cbab381bb
Elizabeth Hall <elizabethhall205@gmail.com>
2016-10-13 17:00:39 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -4,11 +4,18 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if(age >= 13 && age<= 19) {
+  return "You are a teenager!"} else {
+  return "You are not a teenager!"}
 }
 
 function ageChecker(age) {
-
+  if(age >=13 && age <=19) {
+  return "You are a teenager!"; }
+  else if (age <= 12) {
+    return  "You are a kid" }
+  else if (age >= 20) {return "You are a grownup"}
+  else {undefined}
 }
 
 function ternaryTeenager(age) {
