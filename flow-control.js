f587db30dab077bb99ef280b4fe7ed80e3a0a354
Elizabeth Hall <elizabethhall205@gmail.com>
2016-10-14 23:06:02 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,4 @@
-/*function basicTeenager(age) {
+function basicTeenager(age) {
   if(age >= 13 && age<= 19) {
   return "You are a teenager!";}
 }
@@ -8,7 +8,6 @@ function teenager(age) {
   return "You are a teenager!"} else {
   return "You are not a teenager!"}
 }
-*/
 function ageChecker(age) {
   if(age >=13 && age <=19) {
     return "You are a teenager!"; }
