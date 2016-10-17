03e944c2cf4b5d408f04a77500dc752ed504b95c
Laziza Kodirova <kodirova.laziza@gmail.com>
2016-10-17 14:16:38 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,29 +1,38 @@
 function basicTeenager(age) {
-if (age<19 && age>13) {return "You are a teenager!"}
+if (age<=19 && age>=13) {return 'You are a teenager!';}
+return undefined ;
 }
 
 function teenager(age) {
-  if (age<19 && age>13) {return "You are a teenager!"}
-else{
-  return "You are not a teenager"
-}
+  if (age<=19 && age>=13) {return "You are a teenager!";}
+else {return "You are not a teenager";}
 }
 
 function ageChecker(age) {
-if (age<19 && age>13) {return "You are a teenager!"}
-else if (age<12) {return "You are a kid"}
-else {return `"You are a grownup"};
+if (age<=19 && age>=13) {return "You are a teenager!";}
+else if (age<=12) {return "You are a kid";}
+else {return "You are a grownup";}
 }
 
 function ternaryTeenager(age) {
-if (age<19 && age>13) {return "You are not a teenager!"}
-return "You are a teenager"
+
+if (age<=19 && age>=13) {return "You are a teenager!";}
+return "You are not a teenager";
 }
 
+
 function switchAge(age) {
-case (age<19 && age>13):
-return "You are a teenager"
+  switch(age){
+    case 13:
+    case 14:
+     case 15:
+     case 16:
+     case 17:
+     case 18:
+     case 19:
+return "You are a teenager";
 break;
 default:
-return "You have an age"
+return "You have an age";
+}
 }
