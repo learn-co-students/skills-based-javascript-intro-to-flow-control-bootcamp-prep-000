e8bb49d192aa39c9460ce578cac01c97280d3ba9
Andrew Glassett <andrewglassett@gmail.com>
2016-10-20 11:38:57 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -7,20 +7,17 @@ function basicTeenager(age) {
 function teenager(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  }
-  else {
-    return "You are not a teenager";
+  } else {
+    return "You are not a teenager"
   }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  }
-  else if (age < 13) {
+  } else if (age < 13) {
     return "You are a kid";
-  }
-  else if (age > 19){
+  } else if (age > 19){
     return "You are a grownup";
   }
 }
