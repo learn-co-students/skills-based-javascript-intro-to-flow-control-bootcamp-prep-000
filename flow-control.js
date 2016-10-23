8845ff67c4ace5e38e39f9fecd3399b763f5b38b
Byron Staton <b.dewaynestaton@gmail.com>
2016-10-23 02:05:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,14 @@
-function basicTeenager(age) {
+function basicTeenager (age) {
+  if (age === =>13 && <= 19)
+  return 'You are a teenager!'
 
 }
 
 function teenager(age) {
-
+  if (age === =>13 && <= 19)
+  return "You are a teenager!"
+} else {
+  return "You are not a teenager"
 }
 
 function ageChecker(age) {
