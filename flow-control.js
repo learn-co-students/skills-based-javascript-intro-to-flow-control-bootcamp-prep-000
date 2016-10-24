96e7233b4a825e5c7fb5e0a95e0ad347073db85a
JOANNA LUBOWIECKA <asia.lluba@gmail.com>
2016-10-24 18:56:14 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,12 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
 if (age >= 13 && age <=19) {
-  return "You are a teenager"
+  return "You are a teenager!"
 }
 else {
   return "You are not a teenager"
@@ -12,8 +14,8 @@ else {
 }
 
 function ageChecker(age) {
-if (age>=13 && age =< 19) {
-  return "You are a teenager"
+if (age>=13 && age <= 19) {
+  return "You are a teenager!"
 }
 else if (age<=12) {
   return "You are a kid"
@@ -23,13 +25,28 @@ else {
 }
 }
 
-/*function ternaryTeenager(age >=13 && age<=19 ?) {
-return "You are a teenager"
+function ternaryTeenager(age) {
+  return(age >=13 && age<=19 ? "You are a teenager" : "You are not a teenager");
 }
-else {
-  return "You are not a teenager"
-}*/
+
 
 function switchAge(age) {
-
+switch(age) {
+  case 13:
+  return "You are a teenager"
+  case 14:
+  return "You are a teenager"
+  case 15:
+    return "You are a teenager"
+    case 16:
+    return "You are a teenager"
+    case 17:
+    return "You are a teenager"
+    case 18:
+    return "You are a teenager"
+    case 19:
+    return "You are a teenager"
+    default:
+    return "You have an age"
+  }
 }
