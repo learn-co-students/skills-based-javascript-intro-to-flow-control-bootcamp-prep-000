91207c834d62efbbc12fa605ad6d440dc81552b7
David Chase <david.chase4@gmail.com>
2016-10-31 19:06:54 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -78,3 +78,17 @@ describe('flow-control', () => {
     })
   })
 })
+
+function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+  else if (age < 13) {
+    return "You are a kid"
+  }
+  else if (age > 19) {
+    return "You are a grownup"
+  }
+}
+
+ageChecker(15)
