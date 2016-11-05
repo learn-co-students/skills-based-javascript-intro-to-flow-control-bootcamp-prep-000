b65c9d7902115f77ddc99433c2d032dc7ca85325
mata mata <kinethiam@gmail.com>
2016-11-05 13:11:01 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  (age>=13 && age<=19)? "You are a teenager" : "You are not a teenager"
-  return "You are a teenager"
+    return (age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
