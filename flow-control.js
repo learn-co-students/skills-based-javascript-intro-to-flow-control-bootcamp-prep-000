ca1a542f214030eebd9c359cbabc085ae73fa80e
Katie Coppage <katcopp@yahoo.com>
2016-11-06 17:32:00 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,22 @@
 function basicTeenager(age) {
-
+  if (age>12 && age<20) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age <=19 && age >= 13) {
+    return "You are a teenager!"
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
+  if (age<=12) {
+    return "You are a kid"
+  }
+
 
 }
 
@@ -17,3 +27,5 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
+
+basicTeenager(14);
