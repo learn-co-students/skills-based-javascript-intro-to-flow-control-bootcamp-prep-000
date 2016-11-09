76dc378e86903c9c29cd9e8911e491968c08024a
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2016-11-09 19:31:17 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,7 @@
 function basicTeenager(age) {
+  if basicTeenager(age) 
+    age<= 19 && age >= 13
+  console.log("You are a teenager !")
 
 }
 
@@ -16,4 +19,27 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
 
+}
+if basicTeenager(age) {
+  age<= 19 && age >= 13
+console.log("You are a teenager !")
+  }
+  else if teenager(age) {
+    age<= 19 && age >= 13
+  console.log("You are a teenager !")
+    }
+  else if teenager(age){
+      age <= 1 && age <= 13
+      console.log("You are not a teenager");
+    }
+else if teenager(age){
+  age <=12
+  console.log("You are a kid");
+}
+else if (age) {
+  age >= 20
+  console.log('Your a grownup')
+}
+else {
+  console.log();
 }
