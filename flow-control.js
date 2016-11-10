53eed9c8b39ed354ecb5ae450a1d93fbfad7addb
Robert La Plant <rlaplantjr@sbcglobal.net>
2016-11-10 23:10:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,38 @@
+class FlowControl {
 function basicTeenager(age) {
-
+    if(age < 20 && age > 12){
+      return "You are a teenager!"
+    }
 }
 
 function teenager(age) {
-
+      if(age < 20 && age > 12){return "You are a teenager!"}
+      else{return "You are not a teenager"}
 }
 
 function ageChecker(age) {
-
+    if(age < 20 && age > 12){return "You are a teenager!"}
+    elseif(age < 13){return "You are a kid"}
+    else {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
+  return age < 20 && age > 12 ? "You are a teenager" : "You are not a teenager"
 
 }
 
 function switchAge(age) {
+if(age < 20 && age > 12){age = "teen"}
 
+switch(age)
+{
+  case "teen":
+  age = "You are a teenager"
+  break;
+  default:
+  age = "You have an age"
+
+  return age
+}
+}
 }
