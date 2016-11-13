dfce7339a81e0d332d865b1a6ccf90da0c04c68f
Brittany Hill <brit.hill10@gmail.com>
2016-11-13 14:16:18 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -2,15 +2,14 @@ function basicTeenager(age) {
   if (age <= 19 && age >= 13) {
     return "You are a teenager!";
   }
-
 }
 
 function teenager(age) {
   if (age <= 19 && age >= 13) {
      return "You are a teenager!";
-  } else {
-  return "You are not a teenager";
-}
+  }  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
@@ -25,34 +24,20 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  age <= 19 && age >= 13 ? return "You are a teenager!" : return "You are not a teenager";
+  return age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
   switch (age) {
     case 13:
-      return "You are a teenager";
-      break;
     case 14:
-      return "You are a teenager";
-      break;
     case 15:
-      return "You are a teenager";
-      break;
     case 16:
-      return "You are a teenager";
-      break;
     case 17:
-      return "You are a teenager";
-      break;
     case 18:
-      return "You are a teenager";
-      break;
     case 19:
       return "You are a teenager";
-      break;
     default:
       return "You have an age";
-
   }
 }
