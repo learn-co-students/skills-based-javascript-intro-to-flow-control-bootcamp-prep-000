229761606d47e3a19444458f0b701dc272e431df
awowa <sangdencre@gmail.com>
2016-11-15 11:07:35 -0600

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,24 +1,24 @@
 function basicTeenager(age) {
 	if (age >= 13 && age <= 19) {
-		return "You are a teenager!";
+		return "You are a teenager!"
 	}
 }
 
 function teenager(age) {
 	if (age >= 13 && age <= 19) {
-		return "You are a teenager!";
+		return "You are a teenager!"
 	} else {
-		return "You are not a teenager";
+		return "You are not a teenager"
 	}
-}
+}	
 
 function ageChecker(age) {
 	if (age >= 13 && age <= 19) {
-		return "You are a teenager!";
+		return "You are a teenager!"
 	} else if (age <= 12) {
-		return "You are a kid";
+		return "You are a kid"
 	} else if (age >= 20) {
-		return "You are a grownup";
+		return "You are a grownup"
 	}
 }
 
@@ -35,9 +35,8 @@ function switchAge(age) {
 		case 17:
 		case 18:
 		case 19:
-			return "You are a teenager";
+			return "You are a teenager"
 		default:
-			return "You have an age";
+			return "You have an age"
 	}
-
 }
