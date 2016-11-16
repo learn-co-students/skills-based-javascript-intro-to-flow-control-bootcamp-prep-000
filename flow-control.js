f1b6065c971a096c16a1bced263243903821f954
Daniel Larios <daniel.larios.ny@gmail.com>
2016-11-16 18:06:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age>12 && age<20) {
+      return "You are a teenager!"
+} else if(age<=12) {
+      return "You are a kid"
+} else {
+      return "You are a grownup"
 }
 
 function ternaryTeenager(age) {
