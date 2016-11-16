d45998f9d62083e2db0540bd9c84b2d4f76ea221
Daniel Larios <daniel.larios.ny@gmail.com>
2016-11-16 18:13:49 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,25 +1,25 @@
 function basicTeenager(age) {
   	if (age>12 && age<20) {
-  		return "You are a teenager!"
+  		return "You are a teenager!";
 	}
     return undefined
 }
 
 function teenager(age) {
   if (age>12 && age<20) {
-      return "You are a teenager!"
+      return "You are a teenager!";
     } else {
-      return "You are not a teenager"
+      return "You are not a teenager";
     }
 }
 
 function ageChecker(age) {
   if (age>12 && age<20) {
-      return "You are a teenager!"
+      return "You are a teenager!";
 } else if(age<=12) {
-      return "You are a kid"
+      return "You are a kid";
 } else {
-      return "You are a grownup"
+      return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
