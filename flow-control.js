842f124dc1689407ba6e705c93518aa61a087471
Keith Culbreth <culbrethkeith.92@gmail.com>
2016-11-17 00:39:43 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -17,14 +17,11 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19){
     return "You are a teenager!"
   }
-  elseif (age < 13){
+  else if (age < 13){
     return "You are a kid"
   }
-  elseif (age > 19){
-    return "You are a grownup"
-  }
   else {
-    undefined
+    return "You are a grownup"
   }
 }
 
@@ -34,27 +31,22 @@ function ternaryTeenager(age) {
 
 
 function switchAge(age) {
-  case "13":
-    console.log("You are a teenager")
-    break;
-  case "14":
-    console.log("You are a teenager")
-    break;
-  case "15":
-    console.log("You are a teenager")
-    break;
-  case "16":
-    console.log("You are a teenager")
-    break;
-  case "17":
-    console.log("You are a teenager")
-    break;
-  case "18":
-    console.log("You are a teenager")
-    break;
-  case "19":
-    console.log("You are a teenager")
-    break;
+  switch(age){
+  case 13:
+    return "You are a teenager";
+  case 14:
+    return "You are a teenager";
+  case 15:
+    return "You are a teenager";
+  case 16:
+    return "You are a teenager";
+  case 17:
+    return "You are a teenager";
+  case 18:
+    return "You are a teenager";
+  case 19:
+    return "You are a teenager";
   default:
     return "You have an age";
+}
 }
