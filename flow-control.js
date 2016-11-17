8aff54ad3838df6d4eca125388f90b898836fe54
Seth Dornbusch <sethdylan@gmail.com>
2016-11-17 01:25:59 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -2,31 +2,30 @@ function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
  		    return "You are a teenager!"
 		  };
-
 };
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
- 		    return "You are a teenager!"
+ 	  return "You are a teenager!"
  		  }
- 		  else {
- 		    return "You are not a teenager"
+ 	else {
+ 		return "You are not a teenager"
  		  }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19){
-		    return "You are a teenager!";
+	  return "You are a teenager!";
  		  }
- 	elseif (age < 13){
- 		    return "You are a kid";
+ 	else if (age < 13){
+ 	  return "You are a kid";
  		  }
- 	elseif (age > 19){
- 		    return "You are a grownup";
+ 	else if (age > 19){
+ 		return "You are a grownup";
  		  }
-        else {
-        undefined
-}
+  else {
+    undefined
+      }
 
 }
 
@@ -37,27 +36,28 @@ function ternaryTeenager(age) {
 
 
 function switchAge(age) {
-  case 13:
-    console.log("You are a teenager");
+  switch(age) {
+    case 13:
+      return "You are a teenager"
     break;
-  case 14:
-  console.log("You are a teenager");
-  break;
-  case 15:
-  console.log("You are a teenager");
-  break;
-  case 16:
-  console.log("You are a teenager");
-  break;
-  case 17:
-  console.log("You are a teenager");
-  break;
-  case 18:
-  console.log("You are a teenager");
-  break;
-  case 19:
-  console.log("You are a teenager");
-  break;
-  default:
-    return ('You have an age');};
+    case 14:
+      return "You are a teenager"
+    break;
+    case 15:
+      return "You are a teenager"
+    break;
+    case 16:
+      return "You are a teenager"
+    break;
+    case 17:
+      return "You are a teenager"
+    break;
+    case 18:
+      return "You are a teenager"
+    break;
+    case 19:
+      return "You are a teenager"
+    break;
+    default:
+      return ('You have an age');}
 }
