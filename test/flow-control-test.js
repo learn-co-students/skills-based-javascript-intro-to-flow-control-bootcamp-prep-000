e47f976142146629b30f9ca86d060420038e90c2
Antony West <antonywest.design@gmail.com>
2016-12-01 10:32:09 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -78,3 +78,59 @@ describe('flow-control', () => {
     })
   })
 })
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+}
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+  else {
+    return "you are not a teenager";
+  }
+}
+function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age >= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
+}
+function ternaryTeenager(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
+  return "You are not a teengaer";
+}
+function switchAge(age) {
+  switch(age){
+      case (age = 13):
+        console.log("You are a teenager!");
+        break;
+      case (age = 14):
+        console.log("You are a teenager!");
+        break;
+      case (age = 15):
+        console.log("You are a teenager!");
+        break;
+      case (age = 16):
+        console.log("You are a teenager!");
+        break;
+      case (age = 17):
+        console.log("You are a teenager!");
+        break;
+      case (age = 18):
+        console.log("You are a teenager!");
+        break;
+      case (age = 19):
+        console.log("You are a teenager!");
+        break;
+      default:
+        console.log("You are not a teenager");
+        
+    }
+}
