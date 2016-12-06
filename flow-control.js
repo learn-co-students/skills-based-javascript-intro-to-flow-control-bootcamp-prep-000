4ab1a92a1eefb39677a5f19481e375cca0dd2783
Andre M <andre.mueller@zoho.com>
2016-12-06 20:59:37 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,19 +28,13 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch (age) {
-    case (13):
-      return `You are a teenager`
-    case (14):
-      return `You are a teenager`
-    case (15):
-      return `You are a teenager`
-    case (16):
-      return `You are a teenager`
-    case (17):
-      return `You are a teenager`
-    case (18):
-      return `You are a teenager`
-    case (19):
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
       return `You are a teenager`
     default:
       return `You have an age`
