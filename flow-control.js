bb6fea7a6cf0c82118d96ad3885acd63a3cc9f1d
veena penugonda <veenav46@gmail.com>
2016-12-09 00:13:19 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,14 @@
 function basicTeenager(age) {
-
+  if(13 < age < 19){
+    conssole.log("You are a teenager!")
+  }
+  else{
+    console.log("not Teenager")
+  }
+return age
 }
 
 function teenager(age) {
-
 }
 
 function ageChecker(age) {
@@ -11,6 +16,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
+
+return age ? you are teenager : you are not teenager
 
 }
 
