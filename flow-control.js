819388dc0cb456fb94c904259456403dc3cfb0ba
Tracy Chow <tracychow@gmail.com>
2016-12-12 01:40:58 +0000

    Old crap no working

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,12 @@
 function basicTeenager(age) {
-
+  if (age < 13) || (age > 19) {
+    return = "You are not a teenager.";
+  } else (age >= 13) && (age <= 19) {
+    return = "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
 }
 
 function ageChecker(age) {
@@ -15,5 +18,12 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+case "age <= 19":
+console.log("You are a teenager.");
+break;
+case "age >= 20":
+console.log("You are not a teenager.");
+break;
+default:
+console.log("You have an age.")'
 }
