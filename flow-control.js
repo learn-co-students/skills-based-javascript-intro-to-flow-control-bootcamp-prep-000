ea5539f10abdc95e36a212c9c83b3ac74557d5b9
Tranee Peevy <peevy.t@gmail.com>
2016-12-31 23:44:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,54 @@
-function basicTeenager(age) {
 
+function basicTeenager(age = 15) {
+  if (age >= 13 && age <= 19)
+   {
+     return "You are a teenager!";
+   }
+ }
+
+function teenager(age = 15) {
+  if (age >= 13 && age <= 19)
+    {
+      return "You are a teenager!";
+    }
+  else
+    {
+      return "You are not a teenager";
+    }
 }
 
-function teenager(age) {
-
+function ageChecker(age = 15) {
+  if (age >= 13 && age <= 19)
+    {
+       return "You are a teenager!";
+    }
+  else if (age < 13)
+    {
+      return "You are a kid";
+    }
+  else if (age > 19)
+    {
+      return "You are a grownup";
+    }
 }
 
-function ageChecker(age) {
-
+function ternaryTeenager(age = 17) {
+  return age >= 13 && age <= 19 ? "You are a teenager":"You are not a teenager";
 }
 
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function switchAge(age = 15) {
+    switch(age){
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+return "You are a teenager";
+break;
+default:
+  return "You have an age";
+break;
+  }
 }
