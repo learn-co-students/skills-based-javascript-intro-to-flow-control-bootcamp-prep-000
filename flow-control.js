26ec1dae01d23f9d595380af69996d777d0b8b27
Vivian Kulumba <vkulumba@gmail.com>
2017-01-05 01:11:19 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -5,12 +5,12 @@ function basicTeenager (age){
 }
 
 
-function teenager (age)
-{if (age >= 13 && age <= 19)
-  {return 'You are a teenager';
-} else {
-  return 'You are not a teenager!';}
-
+function teenager (age){
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager';
+  } else {
+    return 'You are not a teenager!';
+  }
 }
 
 
