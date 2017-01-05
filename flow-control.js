72be6552bab330c77ea738e431aafd9510284234
Vivian Kulumba <vkulumba@gmail.com>
2017-01-05 01:16:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,8 +28,9 @@ function ageChecker (age) {
   }
 }
 
-function ternaryTeenager (age)
-{return age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager';}
+function ternaryTeenager (age){
+  return age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager';
+}
 
 
 function switchAge(age) {
