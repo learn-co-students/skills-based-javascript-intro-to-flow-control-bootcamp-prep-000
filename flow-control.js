fde218d18af9e1678439b8b0aa402fdd9b63fc1d
Vivian Kulumba <vkulumba@gmail.com>
2017-01-05 01:07:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,17 +15,18 @@ function teenager (age)
 
 
 function ageChecker (age) {
-  if (age >= 13 && age <= 19)
-{return 'You are a teenager';}
-} else {
-  if (age <= 12)
-{return 'You are a kid!';}
-} else {
-  if (age < 19)
-{return 'You are a grownup!';}
-
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager';
+  } else {
+    if (age <= 12){
+      return 'You are a kid!';
+    } else {
+      if (age < 19){
+        return 'You are a grownup!';
+      }
+    }
+  }
 }
-
 
 function ternaryTeenager (age)
 {return age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager';}
