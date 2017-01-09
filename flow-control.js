a775dc0dff758f074a6aad9072b2a249f4810a4a
Joshua Thomas <alwaysimprove@mail.com>
2017-01-09 01:01:15 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,16 @@
-function basicTeenager(age) {if (age >= 13 && age <= 19) {console.log("You are a teenager!");}
+function basicTeenager(age) {if (age >= 13 && age <= 19) {return("You are a teenager!");}
 }
 
 function teenager(age) {if (age <= 19 && age >= 13){
-        console.log("You are a teenager!");
-  }
-  else{
+        return("You are a teenager!");}
+  else {
     return ("You are not a teenager");
  }
 }
 
-function ageChecker(age) {if (age >= 13 && age <= 19) {console.log ('You are a teenager!');
-  } else if (age <= 12) { console.log ('You are a kid');
-                        } else { console.log ('You are a grownup') }
+function ageChecker(age) {if (age >= 13 && age <= 19) {return ('You are a teenager!');
+  } else if (age <= 12) { return ('You are a kid');
+} else { return ('You are a grownup') }
 }
 
 function ternaryTeenager(age) {return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
@@ -32,4 +31,3 @@ function switchAge(age) {
     return 'You have an age';
  }
 }
-
