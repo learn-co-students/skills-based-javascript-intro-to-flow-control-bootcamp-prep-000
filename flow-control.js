aaf9a7605d0a76b6eecfd2111ae3dd5bae9baeaf
Charles Di Renzo <charlesdirenzo@gmail.com>
2017-01-09 16:40:04 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return ((age >= 13) && (age <= 19)) ? "you are a teenager":
+return ((age >= 13) && (age <= 19)) ? "You are a teenager":
   "You are not a teenager"
 }
 
@@ -31,25 +31,19 @@ function switchAge(age) {
   switch(age) {
   case 13:
     return ("You are a teenager");
-  break;
   case 14:
     return ("You are a teenager");
-  break;
   case 15:
     return ("You are a teenager");
-  break;
   case 16:
     return ("You are a teenager");
-  break;
   case 17:
     return ("You are a teenager");
-  break;
   case 18:
     return ("You are a teenager");
-  break;
   case 19:
     return ("You are a teenager");
-  break;
   default:
     return "You have an age"
+  }
 }
