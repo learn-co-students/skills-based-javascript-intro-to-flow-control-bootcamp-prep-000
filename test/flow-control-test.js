4794dd879081ba14b02d1e7a9f5d99e616549982
Kaustubh  Rane <kaustubhrane46@gmail.com>
2017-01-12 13:06:10 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -26,7 +26,10 @@ describe('flow-control', () => {
       expect(basicTeenager(13)).toEqual("You are a teenager!");
     })
 
-    it('should return undefined if the age is not between 13-19', () => {
+    if (age <19) {
+      prit ('you are a teenager')
+      
+    }('should return undefined if the age is not between 13-19', () => {
       expect(basicTeenager(12)).toBe(undefined)
     })
 
