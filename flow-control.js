88e68697f802535e93314a921312abafa7b98df9
Juha  Bach <cdalsoniii@gmail.com>
2017-01-13 12:18:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,22 @@
-function basicTeenager(age) {
+switch(age) {
+  if  (age <= 19) {
+    return basicTeenager(age);
+  }
+}
+function basicTeenager() {
+if(age <= 12){
 
+  return "You are a kid";
+
+} else if(age >= 13 && =< 19 )
+{
+    return "You are a teenager!";
+
+} else if (age >= 20 ){
+
+    return "You are a grownup";
+
+}
 }
 
 function teenager(age) {
