8b0b476c84db878875cc92907db3488d16ff5876
charlie stopek <cstopek@hotmail.com>
2017-01-14 20:03:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,16 @@
 function basicTeenager(age) {
-
+  if (12 < age && age < 20)
+  return('You are a teenager!')
+else console.log(false)
 }
 
 function teenager(age) {
-
+  if (12 < age && age < 20)
+return('You are a teenager!')
+else return("You are not a teenager")
 }
+
+
 
 function ageChecker(age) {
 
