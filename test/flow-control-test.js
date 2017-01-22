b64521a63cb435f7d168130c68a9b1fb73c00ecd
Nicole Therien <nicoletherien@yahoo.com>
2017-01-22 01:06:00 +0000

    Done.

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -22,11 +22,11 @@ describe('flow-control', () => {
   })
 
   describe('basicTeenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
+    if('should return "You are a teenager!" if the age is between 13-19', () => {
       expect(basicTeenager(13)).toEqual("You are a teenager!");
     })
 
-    it('should return undefined if the age is not between 13-19', () => {
+    if('should return undefined if the age is not between 13-19', () => {
       expect(basicTeenager(12)).toBe(undefined)
     })
 
