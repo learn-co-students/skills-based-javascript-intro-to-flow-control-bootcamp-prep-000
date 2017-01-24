aae44860c95805467e0a6fad8ed6af6949ebbbf1
Sarah Skinner <s.c.skinner@gmail.com>
2017-01-24 04:33:37 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,14 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19) {
+  if (age >= 13 && age <= 19) }
+  {
   return 'You are a teenager!';
 }
 
 
 function teenager(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager!";
+  if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager!"; }
   else {
     return "You are not a teenager";
   }
@@ -14,7 +16,7 @@ function teenager(age) {
 
 function ageChecker(age) {
   if(age >= 13 && age <= 19) {
-    return "You are a teenager!";
+    return "You are a teenager!"; }
   else if (age <= 12) {
     return "You are a kid";
   }
