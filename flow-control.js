fea20a6a321890e0fa41e7d68ac88a185b96a3d1
Tayeb Mc <gomail654@gmail.com>
2017-01-26 19:34:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,58 +1,57 @@
-function basicTeenager(age) {
-  if (age>=13 or age<= 19)
-}
-return("You are a teenager!");
-
-function teenager(age)
-     teenager(age) {
-  if (age>=13 or age<= 19)
-}
-return("You are a teenager!");
-else {
- return("You are not a teenager");
-
+function basicTeenager(age){
+  if (age>=13 && age<=19){
+    return("You are a teenager!")
+  }
 }
 
-function ageChecker(age) {
-  if (age>=13 or age<= 19)
-    }
-     return("You are a teenager");
-     else if (age<=13) {
-       return("You are a kid")
-       else {
-         return("You are gorownup");
-       }
+function teenager(age){
+  if (age>=13 && age<=19){
+   return("You are a teenager!")
    }
+else
+  return("You are not a teenager")
+}
 
+function ageChecker(age){
+  if (age>=13 && age<= 19){
+     return("You are a teenager!")
+   }
+     else if (age<=12){
+       return("You are a kid")
+   }
+       else (age>=20)
+         return("You are a grownup")
+}
 
-function ternaryTeenager(age) {
-  if (age>=13 or age<= 19){
-         return true
-         console.log("you are a teenager");
-         else {
-           return false
-           console.log("you are not a teenager");
-         }
-     }
+function ternaryTeenager(age){
+      (age>=13 && age<=19)?console.log("You are a teenager"):console.log("You are not a teenager")
  }
 
+ function switchAge(age){
+ switch("age") {
 
-function switchAge(age) {
-  switch(age) {
-       case "13":
-         return "You are a teenager "
-       case "14":
-         return "You are a teenager "
-       case "15":
-         return "You are a teenager "
-       case "16":
-          return "You are a teenager"
-       case "17":
-       case "18":
-         return "You are a teenager "
-       case "19":
-         return "You are a teenager "
-       default:
-         return " You have an age"
-     }
+  case "13":
+           return"You are a teenager"
+
+ case "14":
+           return"You are a teenager"
+
+  case "15":
+           return"You are a teenager"
+
+  case "16":
+           return"You are a teenager"
+
+  case "17":
+           return"You are a teenager"
+
+  case "18":
+           return"You are a teenager"
+
+  case "19":
+           return"You are a teenager"
+
+      default:
+           return"You have an age"
+      }
  }
