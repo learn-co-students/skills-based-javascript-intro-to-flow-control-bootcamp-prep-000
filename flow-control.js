1fe0e96271289dc08575c3581ac6329d3c5afc1c
Charlay Davenport <charlayd1993@gmail.com>
2017-01-27 01:32:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -51,8 +51,6 @@ function switchAge(age) {
     case "19":
     console.log("You are a teenager");
     break;
-    default:
-    console.log("You have an age");
-    
-  }
+    return "You have an age"
+    }
 }
