19b31b6caf6274a33745c9f289f4be35ef168056
Charlay Davenport <charlayd1993@gmail.com>
2017-01-27 01:09:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,8 +25,7 @@ if (age >=13 && age<=19) {
 }
 
 function ternaryTeenager(age) {
-  return age>=13 && age <=19 ? "You are a teenager":"You are not a teenager" 
-  }
+  return age>=13 && age <=19 ? "You are a teenager":"You are not a teenager"
 }
 
 function switchAge(age) {
