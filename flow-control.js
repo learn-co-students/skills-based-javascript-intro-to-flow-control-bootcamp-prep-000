3594f73ec13c209814e8ddf87ee322f3ec723c4e
Charlay Davenport <charlayd1993@gmail.com>
2017-01-27 01:23:55 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -29,5 +29,30 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case "13":
+    console.log("You are a teenager");
+    break;
+    case "14":
+    console.log("You are a teenager");
+    break;
+    case "15":
+    console.log("You are a teenager");
+    break;
+    case "16":
+    console.log("You are a teenager");
+    break;
+    case "17":
+    console.log("You are a teenager");
+    break;
+    case "18":
+    console.log("You are a teenager");
+    break;
+    case "19":
+    console.log("You are a teenager");
+    break;
+    default:
+    return "You have an age"
+    
+  }
 }
