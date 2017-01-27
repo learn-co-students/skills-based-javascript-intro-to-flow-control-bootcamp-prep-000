db160ecb05b6f10676915dde1532b49f74d1c511
Sarah Denaci <sarahdenaci@gmail.com>
2017-01-27 23:05:57 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,14 @@
-function basicTeenager(age) {
 
+function basicTeenager(age) {
+  if (13<=age<=19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function teenager(age) {
+  (i)
 
 }
 
