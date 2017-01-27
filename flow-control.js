53a24b5cbb776af7f709f17a005e087078446561
Joseph Doyle <j.doyle345@gmail.com>
2017-01-27 23:52:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -27,17 +27,17 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  /*switch(age) {
-    case age>=13 && age<=19:
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
     return "You are a teenager"
     break;
     default:
     return "You have an age"
   }
-}*/
-
-    if (age<=19 && age>=13) {
-      return "You are a teenager"
-    } else {
-      return "You have an age"}
 }
