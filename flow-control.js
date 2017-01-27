093f2a5110e23409e1956577c29e9d8d198cab79
Charlay Davenport <charlayd1993@gmail.com>
2017-01-27 00:02:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,12 @@
 function basicTeenager(age) {
-if (age >= 13 && age <=19) {
+if (age >= 13 && age<=19) {
   return "You are a teenager!"
   }
 }
 
 function teenager(age) {
-if (age >= 13 && age <=19) {
-  return "You are a teenager!"
-} else {
-  return "You are not a teenager"
-}
 
+}
 
 function ageChecker(age) {
 
