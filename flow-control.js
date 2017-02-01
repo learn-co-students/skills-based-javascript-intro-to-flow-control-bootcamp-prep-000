855ef04ea6f5daa77c9ac50a1380c5d4d27745a9
Devindra Madnani <dmadnani@outlook.com>
2017-02-01 19:36:32 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,10 @@
-function teenager(age) {
-if (age >= 13 && <= 19)
-{
+function basicTeenager(age){
+  if (age >= 13 && age <= 19)
+  {return "You are a teenager!"}
+}
+
+ teenager(age) {
+if (age >= 13 && age <= 19) {
   return "You are a teenager!"
 }
 else return {
@@ -9,5 +13,19 @@ else return {
 }
 
 function ternaryTeenager(age){
-  return (age >= 13 && <= 19 ? "You are a teenager" : "You are not a teenager")
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
+}
+
+function ageChecker(age) {
+  if {
+    (age <= 12)
+    {
+  return "You are a kid"
+}
+else if (age >= 19) {
+      return "You are a grownup"
+}
+else {return "You are a teenager!"}
+}
+
 }
