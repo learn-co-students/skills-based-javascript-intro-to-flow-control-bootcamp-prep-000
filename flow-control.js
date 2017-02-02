4784428203dc16ee7fafaa312699926cf00d78b7
priyanka parikh <priyanka.parikh6@gmail.com>
2017-02-02 21:49:02 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,7 @@
-
-
 function basicTeenager(age) {
-  if (age=>13 && age<=19) {
-    return('You are a teenager!');
-  } //end if
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
