3336752161d049c8e34e74b80b7e5f30430c2441
Desislava Todorova <dslavatodorova@gmail.com>
2017-02-06 05:31:41 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,50 @@
 function basicTeenager(age) {
 
+  if (age >= 13 && age <= 19 ) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19 ) {
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19 ) {
+    return "You are a teenager!";
+  }
+  else if (age <= 12) {
+    return "You are a kid";
+  }
+  else if (age >= 20) {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
 
 }
 
 function switchAge(age) {
-
+  var returnValue;
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      returnValue = "You are a teenager";
+      break;
+    default:
+      returnValue = "You have an age"
+  }
+  return returnValue;
 }
