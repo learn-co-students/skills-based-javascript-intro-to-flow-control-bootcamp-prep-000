ecf168f2e6c5fdbddd79bac93db8c62ba6f66fca
fernanda lehaci <fernanda.lehaci@gmail.com>
2017-02-07 19:51:53 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,7 @@ elseif(age<=12) {"You are a kid"} elseif(age>19) {"You are a grownup"}
 
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {return 13<=age<=19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
