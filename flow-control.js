ccabc206b7a8a5a9ace4df81540e5a6ce565ebeb
nilanjam <nilsorde@outlook.in>
2017-02-07 20:17:33 +0530

    Update flow-control.js

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,18 @@
-function basicTeenager(age) {}
+function basicTeenager(age) {if (age >= 13 && age <=19){return "You are a teenager!"}
 }
 
-function teenager(age)
+function teenager(age) {if (age >= 13 && age <=19) {return "You are a teenager!"} 
+else {return "You are not a Teenager!"}
 }
 
-function ageChecker(age)
-
+function ageChecker(age) {if (age >=13 && age<=19) {return "You are a teenager!"}
+else if (age <=12) {return "you are a kid!"} {return"you are a grownup"}
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {if (age>=13 && age<=19)?
+ "You are a teenager":"You are not a teenager"}
 
-}
-
-function switchAge(age) {
-
+function switchAge(age) {switch (age) 
+{case (if(age >=13 && age <=19)): console.log("You are a teenager") 
+default: console.log ("you are a number") break}
 }
