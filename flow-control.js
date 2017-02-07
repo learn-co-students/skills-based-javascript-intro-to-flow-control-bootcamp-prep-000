1fbd2ba4303093793260d6e10ca9c436fb19dd7d
fernanda lehaci <fernanda.lehaci@gmail.com>
2017-02-07 19:43:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,8 @@ function teenager(age) {
 
 }
 
-function ageChecker(age) {
+function ageChecker(age) {if(13<=age<=19) {"You are a teenager!"}
+elseif(age<=12) {"You are a kid"} elseif(age>19) {"You are a grownup"}
 
 }
 
