70b5d95d65114d4a42186bb90f38b5a5aedfa6ce
Adesegun Adedeji <adesegunadedeji@gmail.com>
2017-02-10 01:39:24 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,31 +24,26 @@ else {
 }
 }
 
+//Ternary Operator
 function ternaryTeenager(age){
 
     return (age >=13 && age <=19)? "You are a teenager" :
     "You are not a teenager"
 }
 
+
 function switchAge(age) {
-  switch(age)
+  switch (age)
   {
   case 13:
-  return ("You are a teenager");
   case 14:
-  return ("You are a teenager");
   case 15:
-  return ("You are a teenager");
   case 16:
-  return ("You are a teenager");
   case 17:
-  return ("You are a teenager");
   case 18:
-  return ("You are a teenager");
   case 19:
-  return ("You are a teenager");
+    return "You are a teenager";
   default:
-  //console.log("Definately not a teenager")
-    return("You have an age");
-
+    return "You have an age"
+  }
 }
