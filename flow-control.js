20093399c3c6a551be0dcd807da9256c9501592f
Monika Williams <monika.m.williams@gmail.com>
2017-02-17 05:45:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,45 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age<= 19){
+return "You are a teenager!";
+}else{
+  return undefined;
 }
+}
+basicTeenager(5)
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19){
+return "You are a teenager!";
+} else {
+  return "You are not a teenager";
+ }
 }
+ teenager(16)
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <=19){
+return "You are a teenager!";
+}else if (age <=12 ){
+  return "You are a kid";
+} else {
+  return "You are a grownup";
 }
+}
+ageChecker(22);
 
 function ternaryTeenager(age) {
-
+if(age >=13 && age <=19){
+  return "You are a teenager";
+} else { return "You are not a teenager";
 }
+}
+ternaryTeenager(14)
 
 function switchAge(age) {
-
-}
+  if(age>=13 && age <=19) {
+    return "You are a teenager";
+  }else {
+      return "You have an age";
+    }
+  }
+switchAge(15)
