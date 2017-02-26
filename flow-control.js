5f44b266f93dc01814cf42bc6f65b067e29c2ed9
Josie Kelley <jcakelley@gmail.com>
2017-02-26 21:17:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,9 @@
 function basicTeenager(age) {
-  
+  if(age >= 13 && age <= 19){
+    //do something
+  return "You are a teenager!"
+  //return 'You are a teenager!'
+  }
 }
 
 function teenager(age) {
