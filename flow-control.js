72d9be61e1f8727725c3e3f7ad087562ba11a4cc
Sophie Engel <sophiewoph@gmail.com>
2017-03-05 01:12:31 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -5,18 +5,18 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age >13 && age <= 19) {
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
-    return "You are not a teeanger";
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
-  if(age<=12){
+  if(age >=13 && age <= 19){
+    return "You are a teenager!";
+  } else if (age < 13) {
     return "You are a kid";
-  } else if (age >=13 && age <= 19) {
-    return "You're a teenager";
   } else {
     return "You are a grownup";
   }
@@ -36,7 +36,7 @@ function switchAge(age) {
     case 17:
     case 18:
     case 19:
-      return "You are a teenager!";
+      return "You are a teenager";
       default:
       return "You have an age"
   }
