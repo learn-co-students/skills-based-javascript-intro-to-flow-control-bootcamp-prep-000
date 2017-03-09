85079be8747acf69beb2182f555c6fc8359dc377
Meghan Lewis <mtlewis25@gmail.com>
2017-03-09 03:18:27 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
 function basicTeenager(age) {
-
+  if ( age >= 13 && age <= 19) {
+    return "You are a teenager"
+  }
 }
 
 function teenager(age) {
-
+  if ( age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else {
+    return "You are not a teenager"
+  }
+  }
 }
 
 function ageChecker(age) {
-
+  if ( age <= 12 ) {
+    return "You are a kid"
+  } else if ( age >= 13 && age <= 19 ) {
+    return "You are a teenager"
+  } else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch(age) {
+    case age >= 13 && age <=19:
+    return "You are a teenager"
+    default: "You have an age"
 
+  }
 }
