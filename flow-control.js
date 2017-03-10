d6ed52506d462e81745a0d56a95e373a4d94e71a
khang khau <kjkhau@gmail.com>
2017-03-10 18:30:07 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -19,14 +19,32 @@ function ageChecker(age) {
   } else if (age <= 12){
     return "You are a kid"
   } else {
-    return "you are a grownup"
+    return "You are a grownup"
   }
 }
 
 function ternaryTeenager(age) {
-
+  age >= 13 && age <= 19 ? (
+    answer = "You are a teenager"
+  ) : (
+    answer = "You are not a teenager"
+  );
+    return answer;
 }
 
-function switchAge(age) {
+// age >= 13 ? "You're older than 13!" : "You're younger than 13"
 
+function switchAge(age) {
+  switch(age) {
+    case 13 :
+    case 14 :
+    case 15 :
+    case 16 :
+    case 17 :
+    case 18 :
+    case 19 :
+      return "You are a teenager";
+    default:
+      return "You have an age";
+        }
 }
