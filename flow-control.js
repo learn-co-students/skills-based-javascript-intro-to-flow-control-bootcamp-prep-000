9a1237e9541e0802276511892cb2a94328c0fa1d
Ilkan Atabay <natabay@ku.edu.tr>
2017-03-13 13:05:45 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,14 +24,22 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  return age > 12 && age < 20 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
   switch(age) {
-    case 13-19:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
       return "You are a teenager";
       break;
+
     default:
-      return "You are not a teenager";
+      return "You have an age";
+    }
 }
