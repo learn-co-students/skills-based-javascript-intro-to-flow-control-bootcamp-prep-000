bc4d7820333d0788b1e01ce566fc01ab75470106
Jin Li <jinbl19@gmail.com>
2017-03-21 05:23:21 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,9 +25,27 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  if(age >= 13 && age <=19) {
+    return "You are a teenager"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return 'You are a teenager'
+      break;
+    default:
+      return 'You are not a teenager'
+      
+  }
 }
+
