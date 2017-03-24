78fe4d5c76d268eb24018095a6ed161a9da5a884
Patty Liu <pattylliu@gmail.com>
2017-03-24 20:19:13 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -9,7 +9,7 @@ else{return('You are not a teenager')}
 
 function ageChecker(age) {
 if(age >= 13 && age <= 19){return('You are a teenager')}
-else if(age <= 12){return('You are not a teenager')}
+else if(age <= 12){return('You are a kid')}
 else{return('You are a grownup')}
 }
 
