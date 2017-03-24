6fdb6409f7154d2b62108766bac7ac49725fbd14
Carlos Ortega Morales <carlos.ortega86@gmail.com>
2017-03-24 04:31:58 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -8,7 +8,7 @@ function teenager(age) {
   if(age > 12 && age < 20 ){
     return "You are a teenager!";
   }else{
-    return "You are not  a teenager";
+    return "You are not a teenager";
   }
 }
 
