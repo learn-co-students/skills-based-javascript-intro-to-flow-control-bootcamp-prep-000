34513df3039664d6a48ac45bc418dfd3546f06bb
Anthony Verdesca <verdesca2@gmail.com>
2017-03-25 22:44:16 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,25 @@
 function basicTeenager(age) {
-
+if age >=13 && age <=19 {
+  return ("You are a teenager!")
+}
 }
 
 function teenager(age) {
-
+  age>=13 && age<=19 {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager" }
 }
 
 function ageChecker(age) {
-
-}
+  age>=13 && age<=19 {
+    return "You are a teenager"
+  } else if (age<13) {
+    return "You are a kid"
+  } else {
+    return "You are grownup"  
+  }
+   }
 
 function ternaryTeenager(age) {
 
