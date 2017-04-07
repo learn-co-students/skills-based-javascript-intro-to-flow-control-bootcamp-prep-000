139a60ae4d80653bd2642dbf1a2f1f536fc0aba0
Michelle Cronin <michellelcronin@gmail.com>
2017-04-07 00:24:39 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,31 @@
-function basicTeenager(age) {
 
-}
 
-function teenager(age) {
+var teenager = function(age){
+if (13<=age && age<=19) {
+    return 'You are a teenager!'}
+else if (age<12) {
+	return 'You are a kid!'}
+else {
+	return 'You are a grownup!'}
+};
 
-}
+teenager(69);
 
-function ageChecker(age) {
 
-}
 
-function ternaryTeenager(age) {
 
-}
+var ternaryTeenager = function(age) {
+return age>=13 && age<=19 ? 'teenager!' : 'older/younger person'}
 
-function switchAge(age) {
+ternaryTeenager(9)
 
-}
+var switchage = function(age){
+	switch(age) {
+		case(13<=age && age<=19):
+      console.log('you are a teenager')
+			break;
+    default:
+			console.log('you have an age')
+			break;
+              }};
+switchage(19)
