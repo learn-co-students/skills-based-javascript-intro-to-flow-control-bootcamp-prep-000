0d778575b422c360020cc457ad28d85e98cb02b5
Tacara Solomon <alleyesoncara@gmail.com>
2017-04-23 06:05:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
-function basicTeenager(age) {
-
+function basicTeenager(age < 13-19) {
+if (age < 13-19) {
+return 'You are a teenager!'
 }
-
+}
 function teenager(age) {
+  if (age < 13-19) {
+    return 'you are a teenager!'
+  } else {
+    return 'you are not a teenager '
+  }
 
 }
 
 function ageChecker(age) {
+  if (age < 13-19) {
+    return 'You are a teenager!'
+  } else if (age >= 12){
+    return 'You are a kid'
+  } else {
+    return 'You are a grownup'
+  }
 
 }
 
 function ternaryTeenager(age) {
+  return (age < 13-19)
+  valueToBeReturnedIfTrue : "You are a teenager"
+  valueToBeReturnedIfFalse : "You are not a teenager"
 
 }
 
-function switchAge(age) {
-
+function switchAge(age)
+var age = "13, 14, 15, 16, 17, 18, 19"
+switch(age) {
+  case "age":
+    return "You are a teenager"}
+    default:
+      {return "You have an age"
 }
